76da6446541ee77c64fc983928779dd46992319c
pratapnea <sereversion@gmail.com>
2021-07-04 19:54:09 +0545

    EmployeeService and its implementation is created

---
+++ src/main/java/com/example/springbootbackend/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.springbootbackend.service;
+
+import com.example.springbootbackend.model.Employee;
+
+public interface EmployeeService {
+	
+	Employee saveEmployee(Employee employee);
+
+}
---
+++ src/main/java/com/example/springbootbackend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.springbootbackend.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.springbootbackend.model.Employee;
+import com.example.springbootbackend.repository.EmployeeRepository;
+import com.example.springbootbackend.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeRepository employeeRepository;
+	
+	
+	/*
+	 * Starting from Spring 4.3, if a class, which is configured as a Spring Bean,
+	 * has only one constructor, the @Autowired annotation can be omitted and Spring
+	 * will use the constructor and inject all necessary dependencies;
+	 */
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+
+
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+
+}
