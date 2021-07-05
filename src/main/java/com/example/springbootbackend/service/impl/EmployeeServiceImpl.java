ac1c04afbb5dbc084576d1aeeba9648e6ad39712
pratapnea <sereversion@gmail.com>
2021-07-05 12:48:59 +0545

    Employee Service Implementation is update for get employee by id method

--- src/main/java/com/example/springbootbackend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/springbootbackend/service/impl/EmployeeServiceImpl.java
@@ -47,9 +47,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	@Override
 	public Employee getEmployeeById(long id) {
-		Optional<Employee> employee = employeeRepository.findById(id);
-		if(employee.isPresent()) return employee.get();
-		else throw new ResourceNotFoundException("Employee", "Id", id);
+//		Optional<Employee> employee = employeeRepository.findById(id);
+//		if(employee.isPresent()) return employee.get();
+//		else throw new ResourceNotFoundException("Employee", "Id", id);
+		
+		return this.employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee", "Id", id) );
+		
 	}
 	
 
